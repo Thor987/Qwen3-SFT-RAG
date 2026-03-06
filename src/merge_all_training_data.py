72667bf0b97f6f1723d3ed7766ffef8dd75bc0d4
Pengfa Li <138980802+Thor987@users.noreply.github.com>
2026-03-06 13:53:28 +0800

    Add files via upload

---
+++ src/merge_all_training_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# 读取 Literature.parquet 文件
+df1 = pd.read_parquet('./collect_data/Literature.parquet')
+df12 = pd.read_json('./collect_data/data.jsonl', lines=True)
+
+# 将df12[text]和df1[text]合并
+# 分别再添加一列source，标识数据来源，分为literature和wiki
+df1['source'] = 'literature'
+df12['source'] = 'wiki'
+
+# 合并两个DataFrame
+merged_df = pd.concat([df1, df12], ignore_index=True)
+with open('./collect_data/merged_data.jsonl', 'w', encoding='utf-8') as f:
+    for record in merged_df.to_dict(orient='records'):
+        f.write(f"{pd.io.json.dumps(record, ensure_ascii=False)}\n")
